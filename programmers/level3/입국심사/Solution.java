b792f0bce6f77e8847920f6f299ed18f6f6093fa
yun <yshyeon102@gmail.com>
2023-01-21 10:16:43 +0900

    프로그래머스 level3 입국심사 성공

--- programmers/level3/입국심사/Solution.java
+++ programmers/level3/입국심사/Solution.java
@@ -1,25 +1,35 @@
 package programmers.level3.입국심사;
 
-import java.util.HashMap;
+import java.util.*;
 
 class Solution {
+    public static void main(String[] args) {
+        Solution sol = new Solution();
+        System.out.println(sol.solution(7, new int[]{10, 10}));
+    }
+
     public long solution(int n, int[] times) {
         HashMap<Integer, Integer> map = new HashMap<>();
-        for (int time : times) {
-            map.put(time, 1 + map.getOrDefault(time, 0));
-        }
-        double ppt = 0;
-        for (int key : map.keySet()) {
-            ppt += (double) map.get(key) / key;
-        }
+        for (int time : times) map.put(time, 1 + map.getOrDefault(time, 0));
 
-        long answer = (long) (n / ppt);
-        for (; ; answer++) {
-            int people = 0;
+        List<Integer> list = new ArrayList<>(map.keySet());
+        Collections.sort(list);
+
+        long small = list.get(0);
+        long big = (long)n * list.get(0);
+
+        while (small < big) {
+            long middle = (small + big) / 2;
+            long people = 0;
             for (int key : map.keySet()) {
-                people += map.get(key) * (answer / key);
-                if (people >= n) return answer;
+                people += map.get(key) * (middle / key);
+            }
+            if (people >= n) {
+                big = middle;
+            } else {
+                small = middle + 1;
             }
         }
+        return small;
     }
 }
